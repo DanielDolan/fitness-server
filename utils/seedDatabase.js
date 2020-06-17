3d1d144f45a754524696a777bcf0fcb86e57711c
Daniel Dolan <danieldolan7@gmail.com>
2020-06-17 15:45:45 -0400

    adding another instance on db

--- utils/seedDatabase.js
+++ utils/seedDatabase.js
@@ -25,6 +25,14 @@ const seedDatabase = async () => {
         recomendedReps: 10
     
     }),
+    Exercise.create({
+      name: "waaa Curl",
+     muscleGroup: "Arms, Tricep",
+     imageUrl: "https://images.unsplash.com/photo-1585342565162-3704ff9b221d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
+      description: "Using a dumbell users should ...",
+      recomendedReps: 10
+  
+  }),
     User.create({ 
       firstName: "Daniel",
       lastName:"Dolan",
