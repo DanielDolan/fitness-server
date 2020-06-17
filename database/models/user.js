90e15e2e3b21af2a45c702908c253b98fc845a03
Daniel Dolan <danieldolan7@gmail.com>
2020-06-17 11:49:58 -0400

    users datbase fully working

--- database/models/user.js
+++ database/models/user.js
@@ -5,7 +5,7 @@ const User = db.define("user", {
   firstName: { type: Sequelize.STRING, allowNull: false },
   lastName: {type: Sequelize.STRING, allowNull: false},
   email: {type: Sequelize.STRING, allNull: false },
-  Weight: {type: Sequelize.INTEGER, allNull: false},
+  weight: {type: Sequelize.INTEGER, allNull: false},
 });
 
 module.exports = User;
