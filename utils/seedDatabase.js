27521140723998b1ebdad471d3c9f7a5a18b191a
Daniel Dolan <danieldolan7@gmail.com>
2020-06-18 13:18:50 -0400

    adding more into datbase

--- utils/seedDatabase.js
+++ utils/seedDatabase.js
@@ -5,7 +5,7 @@ const seedDatabase = async () => {
   await Promise.all([
     Exercise.create({
       name: "Push Ups",
-     muscleGroup: "Back, Chest",
+     muscleGroup: "Chest",
       imageUrl: "https://images.unsplash.com/photo-1544216428-3d4ad849ae40?ixlib=rb-1.2.1&auto=format&fit=crop&w=3900&q=80",
       description: "Users can push ther body in a upwards motion from the ground",
       recomendedReps: 5
@@ -17,11 +17,34 @@ const seedDatabase = async () => {
         description: "Using a bench press, users can push up the bar and slowly repeat",
         recomendedReps: 7
     }),
+    Exercise.create({
+      name: "Pectoral Fly",
+      muscleGroup: "Chest",
+      imageUrl: "https://upload.wikimedia.org/wikipedia/commons/2/24/Dumbbell-flys-2.png",
+      description: "Using a dumbell or machine, users can lay flat, stretching out there arms horrizontally to strech the chest and contract its muscles.",
+      recomendedReps: 7
+  }),
   Exercise.create({
     name: "Side Plank",
    muscleGroup: "Abs",
    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Side_Plank.jpg/900px-Side_Plank.jpg",
     description: "Laying directly on side, hold tightly to feel stretch of obliques.  ",
+    recomendedReps: 10
+  
+  }),
+  Exercise.create({
+    name: "Crunches/Sit Ups",
+   muscleGroup: "Abs",
+   imageUrl: "https://www.publicdomainpictures.net/pictures/280000/velka/woman-doing-sit-ups-1538740104g5q.jpg",
+    description: "Laying with knees in air and back on an angle, push the upper body towards the knee, contracting the abdominals. .  ",
+    recomendedReps: 10
+  
+  }),
+  Exercise.create({
+    name: "Scissor kicks",
+   muscleGroup: "Abs",
+   imageUrl: "https://media.defense.gov/2009/Jun/01/2000561751/-1/-1/0/090527-F-7629T-002.JPG",
+    description: "Laying with back slightly in the air, move the legs in a kicking motion contracting the abdominals ",
     recomendedReps: 10
   
   }),
@@ -54,6 +77,14 @@ const seedDatabase = async () => {
    muscleGroup: "Back",
    imageUrl: "https://images.unsplash.com/photo-1566241142364-df7f478ce55f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
     description: "Using a dumbell or barbell, bend the body to stretch the back, pull the weight back to contract back muscles. ",
+    recomendedReps: 10
+  
+  }),
+  Exercise.create({
+    name: "Pull Ups",
+   muscleGroup: "Back",
+   imageUrl: "https://images.unsplash.com/photo-1520334363269-c1b342d17261?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=674&q=80",
+    description: "Using a bar, pull up the body and hold for 2 seconds to contract the back ",
     recomendedReps: 10
   
   }),
